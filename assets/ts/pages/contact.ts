64d919ab5d74c65a521cbbe7246f48dfaa62a6d4
Dylan Crowley <dcrowdev1025@gmail.com>
2023-02-06 13:46:53 -0500

    initial commit

--- assets/ts/pages/contact.ts
+++ assets/ts/pages/contact.ts
@@ -4,14 +4,31 @@ export default async function main() {
     console.log('Hello, world!');
 
     const contactForm = document.querySelector<HTMLFormElement>('#contact-form');
+
     contactForm?.addEventListener('submit', async e => {
         e.preventDefault();
+        
+        const firstName = document.querySelector<HTMLInputElement>('#firstname')?.value;
+        const lastName = document.querySelector<HTMLInputElement>('#lastname')?.value;
+        const fullName = `${firstName} ${lastName}`;
 
+        const company = document.querySelector<HTMLInputElement>('#orgname')?.value;
+        const email = document.querySelector<HTMLInputElement>('#inputEmail4')?.value;
+        const phone = document.querySelector<HTMLInputElement>('#inputPhonenumber4')?.value;
+
+        console.log(fullName, company, email, phone);
         axios.post('/contacts', {
             // params and what
+            name: fullName,
+            company,
+            email,
+            phone,
+            content: 'this is a test'
         }).then(resp => {
+            console.log(resp)
             console.log('Success!');
             // redirect to success page
-        }).catch(err => alert(err));
+            window.location.href = '#success'
+        }).catch(err => `Error ${err.message}`);
     });
 }
